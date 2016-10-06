eea56cd39e9e1f2b1a174b6089be03e2b9d3b583
Ivan Martinez <vanmartc@gmail.com>
2016-10-06 17:44:16 -0500

    starting with backend developmnet

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/hello'
+
+  mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
+  devise_for :users
+  devise_for :installs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: "home#hello"
+end
