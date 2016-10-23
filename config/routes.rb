4fe3c9408fdf72cf86d3af78344040b64f55c298
Andres Diaz <andreselduende@gmail.com>
2016-10-23 18:26:00 -0500

    Add Doorkeeper API authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-
+  use_doorkeeper
   root to: "home#hello"
   get 'home/hello'
 
