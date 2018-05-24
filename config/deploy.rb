f43b957e9cf7ef224e93373ef9dd1615acf48f7f
Ivan Martinez C <vanmartc@gmail.com>
2018-05-24 06:30:20 -0500

    new hum

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
+#set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 #set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 #set :rbenv_map_bins, fetch(:rbenv_map_bins).to_a.concat(%w(bundle exec sidekiq -d -C ./config/sidekiq.yml))
 
@@ -64,7 +64,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
+      #before 'deploy:restart', 'puma:start'
       invoke 'deploy'
       execute "cd #{fetch(:deploy_to)}/current && foreman start"
 
