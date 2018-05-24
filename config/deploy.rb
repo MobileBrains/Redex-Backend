bb02d5ad521a49abe5fb557fd9d68a0edfc08490
Ivan Martinez C <vanmartc@gmail.com>
2018-05-24 07:08:12 -0500

    new hum

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,7 +66,7 @@ namespace :deploy do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
       invoke 'deploy'
-      execute "cd #{fetch(:deploy_to)}/current && RAILS_ENV='#{fetch(:rails_env)}' bundle exec sidekiq -d -C ./config/sidekiq.yml"
+
 
     end
   end
