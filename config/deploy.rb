dbf9fed03a1e934c06b0bedf5afb326195404bc1
Ivan Martinez C <vanmartc@gmail.com>
2018-05-26 19:49:20 -0500

    lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,6 +65,7 @@ namespace :deploy do
   task :initial do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
+      execute "cd #{fetch(:deploy_to)}/current && RAILS_ENV='#{fetch(:rails_env)}' bundle exec sidekiq -d -C ./config/sidekiq.yml"
       invoke 'deploy'
     end
   end
