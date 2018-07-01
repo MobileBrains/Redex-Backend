4d548da31ec1b94d9b93daf84af482aa435f0ffa
Ivan Martinez C <vanmartc@gmail.com>
2018-06-30 23:54:03 -0500

    One Map for currier locations, see map by charge number, see old charges with dates and charges of the day... requeriments done

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,14 +1,41 @@
 class MapsController < ApplicationController
 
   def deliveryOrders
+      @todayChargesNumbersAndDate = []
+      @notTodayChargesNumbersAndDate = []
+      @items = []
+
       office_id = current_user.mail_delivery_office_id
+      @courriers = User.where(mail_delivery_office_id: office_id).where.not(latitude: nil, longitude:nil ).with_role :Courrier
       @delivery_orders = DeliveryOrder.where.not(latitude: nil, longitude:nil ).where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id)
-      @pendent_orders = DeliveryOrder.where.not(latitude: nil, longitude:nil ).where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id)
+      #@pendent_orders = DeliveryOrder.where.not(latitude: nil, longitude:nil ).where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id)
+
+      @todayChargesNumbers = DeliveryOrder.where(mail_delivery_office_id: office_id).where("created_at >= ?", Time.zone.now.beginning_of_day).select(:charge_number).distinct
+      @todayChargesNumbers.each do |tcharge|
+        @items = DeliveryOrder.where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id).where(charge_number: tcharge.charge_number).select(:charge_number, :delivery_man).distinct
+        @items.each do |item|
+          @todayChargesNumbersAndDate << item
+        end
+      end
+
+      @notTodayChargesNumbers = DeliveryOrder.where(mail_delivery_office_id: office_id).where.not("created_at >= ?", Time.zone.now.beginning_of_day).select(:charge_number).distinct
+      @notTodayChargesNumbers.each do |charge|
+        @items = DeliveryOrder.where.not("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id).where(charge_number: charge.charge_number).select(:created_at, :charge_number, :delivery_man).distinct
+        @items.each do |item|
+          @notTodayChargesNumbersAndDate << item
+        end
+      end
+
       @hash = Gmaps4rails.build_markers(@delivery_orders) do |order, marker|
         marker.lat order.latitude
         marker.lng order.longitude
 
         if order.state == "pendiente"
+          marker.picture({
+              "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=P|FF0000|fff|",
+              "width" => 35,
+              "height" => 42
+            })
           marker.infowindow "Estado: #{order.state}<br>
                             Cargada al mensajero el: #{order.created_at}
                             <hr>
@@ -21,9 +48,9 @@ class MapsController < ApplicationController
 
         if order.state == "entregada"
           marker.picture({
-              "url" => "http://icons.iconarchive.com/icons/icons-land/vista-map-markers/32/Map-Marker-Marker-Outside-Chartreuse-icon.png",
-              "width" => 32,
-              "height" => 32
+              "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=E|70cc29|fff|",
+              "width" => 35,
+              "height" => 42
             })
           marker.infowindow " Estado: #{order.state}<br>
                               Fecha de entrega: #{order.delivered_at}<br>
@@ -37,9 +64,9 @@ class MapsController < ApplicationController
         end
         if order.state == "devolucion"
           marker.picture({
-              "url" => "http://www.myiconfinder.com/uploads/iconsets/32-32-369f997cef4f440c5394ed2ae6f8eecd.png",
-              "width" => 32,
-              "height" => 32
+              "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|e3ba0d|fff|",
+              "width" => 35,
+              "height" => 42
             })
           marker.infowindow " Estado: #{order.state}<br>
                               Razon: #{order.devolution.devolution_reason}<br>
@@ -56,6 +83,19 @@ class MapsController < ApplicationController
 
 
       end
+  end
+
+
+  def chargesByCourrier
+    office_id = current_user.mail_delivery_office_id
+    courrierName = params[:courrierName]
+    @todayChargesNumbers = DeliveryOrder.where.not(latitude: nil, longitude:nil ).where(delivery_man: courrierName).where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id).select(:charge_number).distinct
+    #chargeNumber = DeliveryOrder.where.not(latitude: nil, longitude:nil ).where(delivery_man: courrierName).where("created_at >= ?", Time.zone.now.beginning_of_day).where(mail_delivery_office_id: office_id).select(:charge_number).distinct
+    #@chargeDate = DeliveryOrder.where( char)last.created_at
+
+    respond_to do |response|
+      response.json { render json: @todayChargesNumbers}
+    end
   end
 
   def courriersLocation
@@ -81,7 +121,6 @@ class MapsController < ApplicationController
   def oneCourrierOnly
       courrierName = params[:courrier_name]
       @courrier = User.where(name: courrierName).with_role :Courrier
-      puts("#{@courrier}")
       @hash = Gmaps4rails.build_markers(@courrier) do |courrier, marker|
           marker.lat courrier.latitude
           marker.lng courrier.longitude
@@ -101,6 +140,113 @@ class MapsController < ApplicationController
         response.json { render json: @hash}
       end
   end
+
+  def allCourrierLocations
+    courrierName = params[:courrier_name]
+    courrier = User.where(name: courrierName).last
+    @allCourrierLocations = CourriersLocation.where(user_id: courrier.id).where("created_at >= ?", Time.zone.now.beginning_of_day)
+
+    @hash = Gmaps4rails.build_markers(@allCourrierLocations) do |courrierLocation, marker|
+      marker.lat courrierLocation.latitude
+      marker.lng courrierLocation.longitude
+      if courrierLocation.location_type == "Requested"
+        marker.picture({
+                "url" => "https://cdn3.iconfinder.com/data/icons/facebook-ui-flat/48/Facebook_UI-31-32.png",
+                "width" => 32,
+                "height" => 32
+              })
+        marker.infowindow " Nombre: #{courrier.name} <br>
+                            Ultima Ubicacion: #{courrierLocation.location} <br>
+                            Tipo: #{courrierLocation.location_type}
+                            <hr>
+                            Fecha de ubicacion: #{courrierLocation.created_at}"
+      end
+      if courrierLocation.location_type == "Automatic"
+        marker.picture({
+                "url" => "https://cdn3.iconfinder.com/data/icons/facebook-ui-flat/48/Facebook_UI-30-32.png",
+                "width" => 32,
+                "height" => 32
+              })
+        marker.infowindow " Nombre: #{courrier.name} <br>
+                            Ultima Ubicacion: #{courrierLocation.location} <br>
+                            Tipo: #{courrierLocation.location_type}
+                            <hr>
+                            Fecha de ubicacion: #{courrierLocation.created_at}"
+
+      end
+    end
+
+    respond_to do |response|
+     response.json { render json: @hash }
+    end
+  end
+
+  def ordersByChargeNumber
+    charge_number = params[:chargeNumber]
+    office_id = current_user.mail_delivery_office_id
+    @all_orders = DeliveryOrder.where(mail_delivery_office_id: office_id).where(charge_number: charge_number).where.not(latitude: nil, longitude:nil )
+    #@pending_orders = DeliveryOrder.where(charge_number: charge_number, state: "pendiente").where(mail_delivery_office_id: office_id)
+    #@delivered_orders = DeliveryOrder.where(charge_number: charge_number, state: "entregada").where(mail_delivery_office_id: office_id)
+    #@devolutions = DeliveryOrder.where(charge_number: charge_number, state: "devolucion").where(mail_delivery_office_id: office_id)
+
+    @hash = Gmaps4rails.build_markers(@all_orders) do |order, marker|
+      marker.lat order.latitude
+      marker.lng order.longitude
+      if order.state == "pendiente"
+        marker.picture({
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=P|FF0000|fff|",
+                "width" => 35,
+                "height" => 42
+              })
+
+          marker.infowindow " Estado: #{order.state}<br>
+                              Cargada al mensajero el: #{order.created_at}
+                              <hr>
+                              Guia Interna: #{order.internal_guide} <br>
+                              Destinatario: #{order.destinatary} <br>
+                              Direccion: #{order.address} <br>
+                              <hr>
+                              Mensajero Encargado: #{order.delivery_man} "
+      end
+      if order.state == "entregada"
+        marker.picture({
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=E|70cc29|fff|",
+                "width" => 35,
+                "height" => 42
+              })
+
+          marker.infowindow " Estado: #{order.state}<br>
+                              Cargada al mensajero el: #{order.created_at}
+                              <hr>
+                              Guia Interna: #{order.internal_guide} <br>
+                              Destinatario: #{order.destinatary} <br>
+                              Direccion: #{order.address} <br>
+                              <hr>
+                              Mensajero Encargado: #{order.delivery_man} "
+      end
+      if order.state == "devolucion"
+        marker.picture({
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|e3ba0d|fff|",
+                "width" => 35,
+                "height" => 42
+              })
+
+          marker.infowindow " Estado: #{order.state}<br>
+                              Cargada al mensajero el: #{order.created_at}
+                              <hr>
+                              Guia Interna: #{order.internal_guide} <br>
+                              Destinatario: #{order.destinatary} <br>
+                              Direccion: #{order.address} <br>
+                              <hr>
+                              Mensajero Encargado: #{order.delivery_man} "
+      end
+    end
+
+    respond_to do |response|
+     response.json { render json: @hash }
+    end
+  end
+
 
   def requestedCourrierRoute
 
@@ -148,8 +294,8 @@ class MapsController < ApplicationController
         end
 
     respond_to do |response|
-        response.json { render json: @hash }
-      end
+      response.json { render json: @hash }
+    end
   end
 
 
@@ -168,8 +314,8 @@ class MapsController < ApplicationController
           marker.lng order.longitude
 
           marker.picture({
-                "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|P|000|fff",
-                "width" => 100,
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=P|000|fff|",
+                "width" => 35,
                 "height" => 42
               })
 
@@ -188,8 +334,8 @@ class MapsController < ApplicationController
           marker.lng order.longitude
 
           marker.picture({
-                "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|P|F77|000",
-                "width" => 100,
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=P|FF0000|fff|",
+                "width" => 35,
                 "height" => 42
               })
 
@@ -208,8 +354,8 @@ class MapsController < ApplicationController
           marker.lng order.longitude
 
           marker.picture({
-                "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|E|5a0|000",
-                "width" => 100,
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=E|70cc29|fff|",
+                "width" => 35,
                 "height" => 42
               })
 
@@ -231,8 +377,8 @@ class MapsController < ApplicationController
           marker.lng order.longitude
 
           marker.picture({
-                "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|D|FFA500|000",
-                "width" => 100,
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=D|e3ba0d|fff|",
+                "width" => 35,
                 "height" => 42
               })
 
@@ -268,8 +414,8 @@ class MapsController < ApplicationController
 
         if order.state == "devolucion"
           marker.picture({
-              "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|"+ "#{num.to_s}" +"|FFA500|000",
-              "width" => 100,
+              "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld="+ "#{num.to_s}" +"|e3ba0d|fff|",
+              "width" => 35,
               "height" => 42
             })
 
@@ -285,8 +431,8 @@ class MapsController < ApplicationController
                               Mensajero: #{courrierName}  "
         else
           marker.picture({
-                "url" => "https://chart.googleapis.com/chart?chst=d_bubble_text_small&chld=bb|"+ "#{num.to_s}" +"|5a0|000",
-                "width" => 100,
+                "url" => "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld="+ "#{num.to_s}" +"|5a0|fff",
+                "width" => 35,
                 "height" => 42
               })
           marker.infowindow " Estado: #{order.state} <br>
